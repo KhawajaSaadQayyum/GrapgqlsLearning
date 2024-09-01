041258c9ec8194cd60e452b89ec0e0072a40e05d
saad.qayyum <khawajasaadqayyum4110@gmail.com>
2024-09-01 08:36:38 +0300

    Lecture 1 for graphqls

---
+++ src/main/java/com/saad/graphqlsbank/GraphqlsBankApplication.java
@@ -0,0 +1,14 @@
+package com.saad.graphqlsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlsBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlsBankApplication.class, args);
+        System.out.println("Welcome to the qraphql learning");
+    }
+
+}
---
+++ src/test/java/com/saad/graphqlsbank/GraphqlsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad.graphqlsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlsBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
